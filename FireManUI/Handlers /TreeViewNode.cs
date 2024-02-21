817935cf3afec2bea368443a9ccf0641220fa044
Zain Asif <93377394+Zain-Asif-Falcon@users.noreply.github.com>
2024-02-21 18:49:30 +0500

    Create TreeViewNode.cs

---
+++ FireManUI/Handlers /TreeViewNode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FireManUI.Handlers
+{
+    public class TreeViewNode
+    {
+        public int id { get; set; }
+        public string parent { get; set; }
+        public string text { get; set; }
+        public state state { get; set; }
+    }
+    public class state
+    {
+        public bool opened { get; set; }
+        public bool selected { get; set; }
+        public bool disabled { get; set; }
+    }
+}
