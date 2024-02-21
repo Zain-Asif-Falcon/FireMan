4d06d53b2b83809e6ae355dd45d8a20ee0ea0ae5
Zain Asif <93377394+Zain-Asif-Falcon@users.noreply.github.com>
2024-02-21 18:52:59 +0500

    Create AuthenticToken.cs

---
+++ FireManUI/Utility /AuthenticToken.cs
@@ -0,0 +1,17 @@
+﻿using Domain.ViewModel.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FireManUI.Utility
+{
+    public class AuthenticToken
+    {
+        //public async Task<AuthenticationResponse> getLoggedInUser()
+        //{
+        //    AuthenticationResponse data = await HttpContext.Session.GetObject<AuthenticationResponse>("Token");
+        //    return data;
+        //}
+    }
+}
